3197092778c950756a42603b312bc7529a3bf989
sayaniNimesha <sayaninimesha@gmail.com>
2021-02-17 10:32:35 +0530

    going to commit

---
+++ HelloWorld/src/com/sliit/main/Main.java
@@ -0,0 +1,12 @@
+package com.sliit.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is my first project SLIIT");
+
+	}
+
+}
